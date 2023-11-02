6cf6512fd34fce823661ef1bf8fd4638799f17ea
mahdee18 <mahdeec10@gmail.com>
2023-11-02 15:52:42 +0600

    Hosted Gallery Image On imagebb

---
+++ src/data.js
@@ -0,0 +1,49 @@
+// Gallery images
+export default[
+    {
+        id: 1,
+        img: "https://i.ibb.co/fFGqjrM/image-1.webp"
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/z7SzHSH/image-2.webp",
+        
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/tJC4KnS/image-3.webp"
+    },
+    {
+        id: 4,
+        img: "https://i.ibb.co/bF0rkV4/image-4.webp"
+    },
+    {
+        id: 5,
+        img: "https://i.ibb.co/Myz23yR/image-5.webp"
+    },
+    {
+        id: 6,
+        img: "https://i.ibb.co/KNQ2rrB/image-6.webp"
+    },
+    {
+        id: 7,
+        img: "https://i.ibb.co/CpLgLgG/image-7.webp"
+    },
+    {
+        id: 8,
+        img: "https://i.ibb.co/KhM1rrQ/image-8.webp"
+    },
+    {
+        id: 9,
+        img: "https://i.ibb.co/PzPTkpG/image-9.webp"
+    },
+    {
+        id: 10,
+        img: "https://i.ibb.co/2dkt9rs/image-10.jpg"
+    },
+    {
+        id: 11,
+        img: "https://i.ibb.co/0K2kMfL/image-11.jpg"
+    },
+
+]
