ba2511cf9e7b83bd88ab7caaf1201910d6fe7848
mahdee18 <mahdeec10@gmail.com>
2023-11-06 09:29:26 +0600

    Change Gallery Image Format

--- src/data.js
+++ src/data.js
@@ -1,41 +1,41 @@
 // Gallery images
-export default[
+export default [
     {
         id: 1,
-        img: "https://i.ibb.co/fFGqjrM/image-1.webp"
+        img: "https://i.ibb.co/CsrTcYZ/image-1-removebg-preview.png"
     },
     {
         id: 2,
-        img: "https://i.ibb.co/z7SzHSH/image-2.webp",
-        
+        img: "https://i.ibb.co/tmWppYy/image-2.png",
+
     },
     {
         id: 3,
-        img: "https://i.ibb.co/tJC4KnS/image-3.webp"
+        img: "https://i.ibb.co/f1yFNRk/image-3.png"
     },
     {
         id: 4,
-        img: "https://i.ibb.co/bF0rkV4/image-4.webp"
+        img: "https://i.ibb.co/RzFT8q0/image-4.png"
     },
     {
         id: 5,
-        img: "https://i.ibb.co/Myz23yR/image-5.webp"
+        img: "https://i.ibb.co/gzJgdmG/image-5.png"
     },
     {
         id: 6,
-        img: "https://i.ibb.co/KNQ2rrB/image-6.webp"
+        img: "https://i.ibb.co/mTv4VJS/image-6.png"
     },
     {
         id: 7,
-        img: "https://i.ibb.co/CpLgLgG/image-7.webp"
+        img: "https://i.ibb.co/s3Y3hTD/image-7.png"
     },
     {
         id: 8,
-        img: "https://i.ibb.co/KhM1rrQ/image-8.webp"
+        img: "https://i.ibb.co/JHN85HR/image-8.png"
     },
     {
         id: 9,
-        img: "https://i.ibb.co/PzPTkpG/image-9.webp"
+        img: "https://i.ibb.co/26cQr5h/image-9.png"
     },
     {
         id: 10,
